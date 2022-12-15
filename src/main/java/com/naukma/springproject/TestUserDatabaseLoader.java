52e0966fb6b235237878eec2d222617f478d53ef
reqLane <vlad5000191@gmail.com>
2022-12-15 09:03:16 +0200

    123

--- src/main/java/com/naukma/springproject/TestUserDatabaseLoader.java
+++ src/main/java/com/naukma/springproject/TestUserDatabaseLoader.java
@@ -34,62 +34,62 @@ public class TestUserDatabaseLoader {
         return args -> {
             UserEntity user1 = new UserEntity("anatolii72", passwordEncoder.encode("pass"), "Anatolii", "Andrusenko", Role.STUDENT, "anatolii.andrusenko@ukma.edu.ua");
             UserEntity user2 = new UserEntity("march", passwordEncoder.encode("pass"), "Vladyslav", "Marchenko", Role.ADMIN, "march@gmail.com");
-            UserEntity user3 = new UserEntity("andriy", passwordEncoder.encode("123"), "Andriy", "Videyko", Role.STUDENT, "andriy@ukma.edu.ua");
+            UserEntity user3 = new UserEntity("andriy", passwordEncoder.encode("pass"), "Andriy", "Videyko", Role.STUDENT, "andriy@ukma.edu.ua");
 
             userRepository.saveAll(List.of(user1,user2,user3));
 
             Organization org1 = new Organization();
-            org1.setName("Org_1st");
+            org1.setName("Photo Club");
             Organization org2 = new Organization();
-            org2.setName("Org_2nd");
+            org2.setName("IT Club");
             Organization org3 = new Organization();
-            org3.setName("Org_3rd");
+            org3.setName("Art Club");
             organizationService.register(org1);
             organizationService.register(org2);
             organizationService.register(org3);
 
-            organizationService.addStudent("Org_1st", "andriy");
-            organizationService.addStudent("Org_2nd", "andriy");
-            organizationService.addStudent("Org_3rd", "andriy");
+            organizationService.addStudent("Photo Club", "andriy");
+            organizationService.addStudent("IT Club", "andriy");
+            organizationService.addStudent("Art Club", "andriy");
 
             Project proj1 = new Project();
-            proj1.setName("proj1");
+            proj1.setName("Photographing courses");
             Project proj2 = new Project();
-            proj2.setName("proj2");
+            proj2.setName("Party Evening");
             Project proj3 = new Project();
-            proj3.setName("proj3");
+            proj3.setName("Student Activities");
             Project proj4 = new Project();
-            proj4.setName("proj4");
+            proj4.setName("C++ Courses");
             Project proj5 = new Project();
-            proj5.setName("proj5");
+            proj5.setName("Hackathon Sep2022");
             Project proj6 = new Project();
-            proj6.setName("proj6");
+            proj6.setName("Music Processing Soft");
             Project proj7 = new Project();
-            proj7.setName("proj7");
+            proj7.setName("Painting Courses");
             Project proj8 = new Project();
-            proj8.setName("proj8");
+            proj8.setName("3D Modelling");
             Project proj9 = new Project();
-            proj9.setName("proj9");
+            proj9.setName("Art Exhibition");
 
-            projectService.addTo(proj1, "Org_1st");
-            projectService.addTo(proj2, "Org_1st");
-            projectService.addTo(proj3, "Org_1st");
-            projectService.addTo(proj4, "Org_2nd");
-            projectService.addTo(proj5, "Org_2nd");
-            projectService.addTo(proj6, "Org_2nd");
-            projectService.addTo(proj7, "Org_3rd");
-            projectService.addTo(proj8, "Org_3rd");
-            projectService.addTo(proj9, "Org_3rd");
+            projectService.addTo(proj1, "Photo Club");
+            projectService.addTo(proj2, "Photo Club");
+            projectService.addTo(proj3, "Photo Club");
+            projectService.addTo(proj4, "IT Club");
+            projectService.addTo(proj5, "IT Club");
+            projectService.addTo(proj6, "IT Club");
+            projectService.addTo(proj7, "Art Club");
+            projectService.addTo(proj8, "Art Club");
+            projectService.addTo(proj9, "Art Club");
 
-            projectService.addStudent("proj1", "andriy");
-            projectService.addStudent("proj2", "andriy");
-            projectService.addStudent("proj3", "andriy");
-            projectService.addStudent("proj4", "andriy");
-            projectService.addStudent("proj5", "andriy");
-            projectService.addStudent("proj6", "andriy");
-            projectService.addStudent("proj7", "andriy");
-            projectService.addStudent("proj8", "andriy");
-            projectService.addStudent("proj9", "andriy");
+            projectService.addStudent("Photographing courses", "andriy");
+            projectService.addStudent("Party Evening", "andriy");
+            projectService.addStudent("Student Activities", "andriy");
+            projectService.addStudent("C++ Courses", "andriy");
+            projectService.addStudent("Hackathon Sep2022", "andriy");
+            projectService.addStudent("Music Processing Soft", "andriy");
+            projectService.addStudent("Painting Courses", "andriy");
+            projectService.addStudent("3D Modelling", "andriy");
+            projectService.addStudent("Art Exhibition", "andriy");
         };
     }
 }
